205d551ece1266d3088479277a6cfc59ca1730ce
BRIAN STEFFES <brian@steffes.io>
2015-01-25 12:27:14 -0500

    Incorporating some print functions

--- panagrams.c
+++ panagrams.c
@@ -9,39 +9,49 @@
 #include <string.h>
 #include <stdbool.h>
 #include <ctype.h>
-#include <regex.h>
 
-#define MAX_STRING_LENGTH 10e3
+#define LETTERS_IN_ALPHABET 26
+#define MAX_STRING_LENGTH 1000
 
-struct letter {
+typedef struct {
 	char c_value;
 	bool found;
-} alphabet[26];						/*initialize an array of struct letters*/
+} letter;						
 
+/*FUNCTION PROTOTYPES*/
+int read_string(letter* my_alphabet[], char* my_string);
 
 int main(void)
 {
 	
+	letter alphabet[LETTERS_IN_ALPHABET];
 	/* initialize the alphabet as an organized structure */
-	for(int i = 0, c = 'a'; i < 26; i++, c++)
+	for(int i = 0, c = 'a'; i < LETTERS_IN_ALPHABET; i++, c++)
 	{
 		alphabet[i].c_value = c;
 		alphabet[i].found = false;
 	}
 
-	int letter_count = 0;
+	int letter_count;
 	char current_char;
 	char my_string[MAX_STRING_LENGTH];	
 	/*load the string from StdIN, one character at a time*/\
 	for(int i = 0; !(current_char == '\n' || current_char == EOF); i++)
 	{
 		scanf("%c", &current_char);
-		while(current_char == ' ')		/*Skip SPaces*/
+		while(current_char == ' ')		/*Skip Spaces*/
 			scanf("%c", &current_char);
-		
+		if(current_char == '\n' || current_char == EOF)
+			break;
+		else if(!islower(current_char))
+			current_char = tolower(current_char);
 
-
+		my_string[i] = current_char;
 	}
+
+	printf("String loaded from STDIN:\n\n%s\n", my_string);
+	printf("\n%d\n", strlen(my_string));
+	letter_count = read_string(alphabet, my_string);
 
 	switch(letter_count){
 		case 26:
@@ -53,4 +63,42 @@ int main(void)
 	return 0;
 }
 
+/*read_string:	takes a pointer to an array of letter structures, and a pointer to a char
+		array as arguments. tests each letter in string against structures in array.
+		Returns a count of structs who's member 'found' == true.
+		If returned int is equal to 26 then the string is indeed a panagram.
+*/	
+int read_string(letter* my_alphabet[], char* my_string)
+{	
+	int count;
+	char current_char;
+	for(int i = 0; my_string[i] != '\0'; i++)
+	{
+		current_char = my_string[i];
+		if(my_alphabet[current_char - 'a'] -> found == false)
+		{
+			count++;
+			my_alphabet[current_char - 'a'] -> found = true;
+		}
+	}
+	return count;
+}
+
+/* Funciton specific Function prototype*/
+void print_structure(letter* my_letter);
+
+void print_structure_array(letter* my_alphabet[])
+{
+
+}
+
+void print_structure(letter* my_letter)
+{
+	char my_char = my_letter -> c_value;
+	char* boolean_val = "false";
+	if(my_letter -> found == true)
+		boolean_val = "true";
+
+	printf()
+}
 
