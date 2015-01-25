7257149693c6fae307970cc38a029f8f1a53f57e
BRIAN STEFFES <brian@steffes.io>
2015-01-25 10:58:40 -0500

    panagrams.c

---
+++ panagrams.c
@@ -0,0 +1,30 @@
+/* A Panagram is a string that has at least one instance of every letter in the english alphabet*/
+  
+ #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+typedef struct{
+	char c_value;
+	bool found;
+} letter[26];
+
+/*	FUNCTION PROTYPES	*/
+
+
+
+int main(void)
+{
+	printf("%lu\n", sizeof(letter));
+	/* initialize the alphabet as an organized structure */
+	//for(int i = 0; i < size)
+
+
+
+
+
+
+
+	return 0;
+}
